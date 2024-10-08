75e00f0c8bc794e98b033e619278b0e34a4d86d2
Maxi Sandoval <maxisandoval98@gmail.com>
2024-10-08 20:10:06 -0300

    first commit

---
+++ src/main/java/ar/dev/maxisandoval/equiposspringsecurityapirest/EquiposSpringSecurityApiRestApplication.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.equiposspringsecurityapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposSpringSecurityApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquiposSpringSecurityApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/dev/maxisandoval/equiposspringsecurityapirest/EquiposSpringSecurityApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.equiposspringsecurityapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquiposSpringSecurityApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
